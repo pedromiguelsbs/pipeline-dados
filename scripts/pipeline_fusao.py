7094c41a16ccf9590ca4f802b01c81ef97dfeaac
Pedro Miguel <pedro.miguel@hotmail.com.br>
2025-03-13 22:20:48 -0300

    Refatorando o código e criando funções para ETL

--- scripts/pipeline_fusao.py
+++ scripts/pipeline_fusao.py
@@ -1,9 +1,6 @@
 import json
 import csv
 
-path_json = 'raw_data/dados_empresaA.json'
-path_csv = 'raw_data/dados_empresaB.csv'
-
 def read_json(path):
     with open(path, 'r') as file:
         data = json.load(file)
@@ -42,18 +39,32 @@ def join(data1, data2):
     combined_datas.extend(data2)
     return combined_datas
 
-#1) EXTRACT: lendo os dados
+def standardize_columns(old_data, column_pattern):
+    new_data = [column_pattern]
+    for item in list(old_data):
+        row = []
+        for column in column_pattern:
+            row.append(item.get(column, 'Indisponível'))
+        new_data.append(row)
+    return new_data
+
+def save_data(path, data):
+    with open(path, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+
+#EXTRACT
+#Lendo os dados
+path_json = 'raw_data/dados_empresaA.json'
+path_csv = 'raw_data/dados_empresaB.csv'
 data_json = read_data(path_json, 'json')
 data_csv = read_data(path_csv, 'csv')
-#print(data_json[0])
-#print(data_csv[0])
 
-
-#2) TRANSFORM: padronizando colunas
+#TRANSFORM
+#Padronizando os nomes das colunas do CSV p/ igualar com as do JSON
 columns_json = get_columns(data_json)
 columns_csv = get_columns(data_csv)
-#print(columns_json)
-#print(columns_csv)
 key_mapping = {'Nome do Item' :'Nome do Produto', 
                  'Classificação do Produto': 'Categoria do Produto', 
                  'Valor em Reais (R$)': 'Preço do Produto (R$)',
@@ -62,14 +73,13 @@ key_mapping = {'Nome do Item' :'Nome do Produto',
                  'Data da Venda': 'Data da Venda'}
 new_data_csv = rename_columns(data_csv, key_mapping)
 new_columns_csv = get_columns(new_data_csv)
-#print(new_columns_csv)
+#Unindo os dados
+combined_data = join(data_json, new_data_csv)
 
-#2.1) TRANSFORM: unindo os dados
-combined_datas = join(data_json, new_data_csv)
-#print(combined_datas[0])
-#print(combined_datas[-1])
-
-#2.2) TRANSFORM: Resolvendo a coluna 'Data da Venda' faltante no JSON
-
-
-#3) Load
+#LOAD
+#Resolvendo o problema da coluna faltante 'Data da Venda' no JSON
+new_combined_data = standardize_columns(combined_data, new_columns_csv)
+#Salvando os dados
+path = 'processed_data/dados_combinados.csv'
+save_data(path, new_combined_data)
+print(f"Dados salvos em: {path}")
